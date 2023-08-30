8dd38963937f7e4f4ca3d07ad44a8000806c5285
Shrey0330 <shreyshah5578@gmail.com>
2023-08-30 11:26:23 +0530

    Changes will added

--- src/test/java/com/chandrayan/Chandrrayan3_assement/ChandrayanTest.java
+++ src/test/java/com/chandrayan/Chandrrayan3_assement/ChandrayanTest.java
@@ -33,7 +33,119 @@ public class ChandrayanTest
         Assert.assertEquals(Expected_Y, y);
         Assert.assertEquals(Expected_Z, z);
     }
+    @Test
+    public void testForForward() throws Exception 
+    {
+    	char[] commands = {'f'};
+    	String Expected_Direction="N";
+    	int Expected_X = 0;
+    	int Expected_Y = 1;
+    	int Expected_Z = 0;
+        Chandrayanmain rocket = new Chandrayanmain(0,0,0, "N", commands);
+        rocket.Test();
+        String Direction = rocket.getDirection();
+        int x = rocket.getX();
+        int y = rocket.getY();
+        int z = rocket.getZ();
+       Assert.assertEquals(Expected_Direction, Direction); 
+        Assert.assertEquals(Expected_X, x);
+        Assert.assertEquals(Expected_Y, y);
+        Assert.assertEquals(Expected_Z, z);
+    }
+    @Test
+    public void testForBackward() throws Exception 
+    {
+    	char[] commands = {'b'};
+    	String Expected_Direction="N";
+    	int Expected_X = 0;
+    	int Expected_Y = -1;
+    	int Expected_Z = 0;
+        Chandrayanmain rocket = new Chandrayanmain(0,0,0, "N", commands);
+        rocket.Test();
+        String Direction = rocket.getDirection();
+        int x = rocket.getX();
+        int y = rocket.getY();
+        int z = rocket.getZ();
+       Assert.assertEquals(Expected_Direction, Direction); 
+        Assert.assertEquals(Expected_X, x);
+        Assert.assertEquals(Expected_Y, y);
+        Assert.assertEquals(Expected_Z, z);
+    }  @Test
+    public void testForUp() throws Exception 
+    {
+    	char[] commands = {'u'};
+    	String Expected_Direction="Up";
+    	int Expected_X = 0;
+    	int Expected_Y = 0;
+    	int Expected_Z = 0;
+        Chandrayanmain rocket = new Chandrayanmain(0,0,0, "N", commands);
+        rocket.Test();
+        String Direction = rocket.getDirection();
+        int x = rocket.getX();
+        int y = rocket.getY();
+        int z = rocket.getZ();
+       Assert.assertEquals(Expected_Direction, Direction); 
+        Assert.assertEquals(Expected_X, x);
+        Assert.assertEquals(Expected_Y, y);
+        Assert.assertEquals(Expected_Z, z);
+    }
+    @Test
+    public void testForDown() throws Exception 
+    {
+    	char[] commands = {'d'};
+    	String Expected_Direction="Down";
+    	int Expected_X = 0;
+    	int Expected_Y = 0;
+    	int Expected_Z = 0;
+        Chandrayanmain rocket = new Chandrayanmain(0,0,0, "N", commands);
+        rocket.Test();
+        String Direction = rocket.getDirection();
+        int x = rocket.getX();
+        int y = rocket.getY();
+        int z = rocket.getZ();
+       Assert.assertEquals(Expected_Direction, Direction); 
+        Assert.assertEquals(Expected_X, x);
+        Assert.assertEquals(Expected_Y, y);
+        Assert.assertEquals(Expected_Z, z);
+    }
+    @Test
+    public void testForLeft() throws Exception 
+    {
+    	char[] commands = {'l'};
+    	String Expected_Direction="W";
+    	int Expected_X = 0;
+    	int Expected_Y = 0;
+    	int Expected_Z = 0;
+        Chandrayanmain rocket = new Chandrayanmain(0,0,0, "N", commands);
+        rocket.Test();
+        String Direction = rocket.getDirection();
+        int x = rocket.getX();
+        int y = rocket.getY();
+        int z = rocket.getZ();
+       Assert.assertEquals(Expected_Direction, Direction); 
+        Assert.assertEquals(Expected_X, x);
+        Assert.assertEquals(Expected_Y, y);
+        Assert.assertEquals(Expected_Z, z);
+    }
     
-    
+    @Test
+    public void testForRight() throws Exception 
+    {
+    	char[] commands = {'r'};
+    	String Expected_Direction="E";
+    	int Expected_X = 0;
+    	int Expected_Y = 0;
+    	int Expected_Z = 0;
+        Chandrayanmain rocket = new Chandrayanmain(0,0,0, "N", commands);
+        rocket.Test();
+        String Direction = rocket.getDirection();
+        int x = rocket.getX();
+        int y = rocket.getY();
+        int z = rocket.getZ();
+       Assert.assertEquals(Expected_Direction, Direction); 
+        Assert.assertEquals(Expected_X, x);
+        Assert.assertEquals(Expected_Y, y);
+        Assert.assertEquals(Expected_Z, z);
+    }
 }
 
